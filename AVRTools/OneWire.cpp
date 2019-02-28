1580dc61f586132c0e5110724f4ad766d3310738
alterego@users.com <alterego@users.com>
2019-02-28 23:35:32 +0300

    Add OneWire library. UART mode not working yet

---
+++ AVRTools/OneWire.cpp
@@ -0,0 +1,286 @@
+#include "OneWire.h"
+
+#ifdef UART_AS_OneWire
+//#include <avr/interrupt.h>
+#include "USART1Minimal.h"
+#endif
+
+#ifndef UART_AS_OneWire
+void ow_set(uint8_t mode) {
+#ifndef OW_TWO_PINS
+  if (mode) {
+    setGpioPinLow(OW_ALTER_PIN); //not really necessary
+    setGpioPinModeOutput(OW_ALTER_PIN);
+  } else {
+    setGpioPinModeInput(OW_ALTER_PIN);
+  }
+#else
+  if (mode)
+    setGpioPinLow(OW_ALTER_PIN_OUT);
+  else
+    setGpioPinHigh(OW_ALTER_PIN_OUT);
+#endif
+}
+
+uint8_t ow_checkin(void) {
+#ifndef OW_TWO_PINS
+  // return CheckBit(OW_PIN, OW_BIT);
+  return readGpioPinDigital(OW_ALTER_PIN);
+#else
+  // return CheckBit(OW_PIN, OW_BIT_IN);
+  return readGpioPinDigital(OW_ALTER_PIN_IN);
+#endif
+}
+
+#endif
+
+uint8_t ow_reset(void) {
+#ifdef UART_AS_OneWire
+  // UCSRB = (1 << RXEN) | (1 << TXEN);
+  //// 9600
+  // UBRRL = USART_BAUDRATE_9600;
+  // UBRRH = (USART_BAUDRATE_9600 >> 8);
+  // UCSRA &= ~(1 << U2X);
+  //
+  // while (CheckBit(UCSRA, RXC))
+  // UDR; //clear buffer
+  // cli();
+  // UDR = 0xF0;
+  // UCSRA = (1 << TXC);
+  // sei();
+  //// while(!CheckBit(UCSRA, TXC)) OthersTasks();
+  // while (!CheckBit(UCSRA, RXC))
+  // OthersTasks();
+  // if (UDR != 0xF0)
+  // return 1;
+  // return 0;
+  uint8_t d = 0xF0;
+  initUSART1(9600);
+  UCSR1A &= ~(1 << U2X1);
+  transmitUSART1(d);
+  return (d != receiveUSART1());
+
+#else
+  uint8_t status;
+  ow_set(1);
+  _delay_us(480);
+  ow_set(0);
+  _delay_us(60);
+  // Store line value and wait until the completion of 480uS period
+  status = ow_checkin();
+  _delay_us(420);
+  // Return the value read from the presence pulse (0=OK, 1=WRONG)
+  return !status;
+#endif
+  //	return 1 if found
+  //	return 0 if not found
+}
+
+void ow_write_bit(uint8_t bit) {
+#ifdef UART_AS_OneWire
+  //// 115200
+  // UBRRL = USART_BAUDRATE_115200;
+  // UBRRH = (USART_BAUDRATE_115200 >> 8);
+  // UCSRA |= (1 << U2X);
+  //
+  // uint8_t d = 0x00;
+  // while (CheckBit(UCSRA, RXC))
+  // UDR; //clear buffer
+  // if (bit)
+  // d = 0xFF;
+  // cli();
+  // UDR = d;
+  // UCSRA = (1 << TXC);
+  // sei();
+  // while (!CheckBit(UCSRA, TXC))
+  //;
+  // while (CheckBit(UCSRA, RXC))
+  // UDR; //clear buffer
+  uint8_t d = 0x00;
+  if (bit)
+    d = 0xFF;
+  initUSART1(115200);
+  UCSR1A |= (1 << U2X1); // double speed ?
+  transmitUSART1(d);
+  // releaseUSART1();
+#else
+  // Pull line low for 1uS
+  ow_set(1);
+  _delay_us(1);
+  // If we want to write 1, release the line (if not will keep low)
+  if (bit)
+    ow_set(0);
+  // Wait for 60uS and release the line
+  _delay_us(60);
+  ow_set(0);
+#endif
+}
+
+uint8_t ow_read_bit(void) {
+#ifdef UART_AS_OneWire
+  //// 115200
+  // UBRRL = USART_BAUDRATE_115200;
+  // UBRRH = (USART_BAUDRATE_115200 >> 8);
+  // UCSRA |= (1 << U2X);
+  //
+  // uint8_t c;
+  // while (CheckBit(UCSRA, RXC))
+  // UDR; //clear buffer
+  // cli();
+  // UDR = 0xFF;
+  // UCSRA = (1 << TXC);
+  // sei();
+  // while (!CheckBit(UCSRA, TXC))
+  //;
+  // while (!CheckBit(UCSRA, RXC))
+  //;
+  // c = UDR;
+  // if (c > 0xFE)
+  // return 1;
+  // return 0;
+  uint8_t d;
+  initUSART1(115200);
+  UCSR1A |= (1 << U2X1); // double speed?
+  d = receiveUSART1();
+  if (d > 0xFE)
+    return 1;
+  return 0;
+#else
+  uint8_t bit = 0;
+  // Pull line low for 1uS
+  ow_set(1);
+  _delay_us(1);
+  // Release line and wait for 14uS
+  ow_set(0);
+  _delay_us(14);
+  // Read line value
+  if (ow_checkin())
+    bit = 1;
+  // Wait for 45uS to end and return read value
+  _delay_us(45);
+  return bit;
+#endif
+}
+
+#ifdef UART_AS_OneWire
+uint8_t ow_write_byte(uint8_t byte) {
+  uint8_t i = 8;
+  //// 115200
+  // UBRRL = USART_BAUDRATE_115200;
+  // UBRRH = (USART_BAUDRATE_115200 >> 8);
+  // UCSRA |= (1 << U2X);
+  //
+  // do {
+  // uint8_t d = 0x00;
+  // if (byte & 1)
+  // d = 0xFF;
+  // cli();
+  // UDR = d;
+  // UCSRA = (1 << TXC);
+  // sei();
+  // OthersTasks();
+  // while (!CheckBit(UCSRA, RXC))
+  // OthersTasks();
+  // byte >>= 1;
+  // if (UDR > 0xFE)
+  // byte |= 128;
+  //} while (--i);
+  initUSART1(115200);
+  UCSR1A |= (1 << U2X1); // double speed
+  do {
+    uint8_t d = 0x00;
+    if (byte & 1)
+      d = 0xFF;
+    transmitUSART1(d);
+    byte >>= 1;
+    if (receiveUSART1() > 0xFE)
+      byte |= 128;
+  } while (--i);
+  return byte & 0xFF;
+}
+#else
+void ow_write_byte(uint8_t b) {
+  for (uint8_t p = 8; p; p--) {
+    ow_write_bit(b & 1);
+    b >>= 1;
+  }
+}
+
+uint8_t ow_read_byte(void) {
+  uint8_t r = 0;
+  for (uint8_t p = 8; p; p--) {
+    r >>= 1;
+    if (ow_read_bit())
+      r |= 0x80;
+  }
+  return r;
+}
+#endif
+
+uint8_t ow_search_rom(uint8_t diff, uint8_t *id) {
+  uint8_t i, j, next_diff;
+  uint8_t b;
+
+  if (!ow_reset())
+    return OW_PRESENCE_ERR; // error, no device found
+
+  ow_write_byte(OW_CMD_SEARCHROM); // ROM search command
+  next_diff = OW_LAST_DEVICE;      // unchanged on last device
+
+  i = OW_ROMCODE_SIZE * 8; // 8 bytes
+  do {
+    j = 8; // 8 bits
+    do {
+      b = ow_read_bit();      // read bit
+      if (ow_read_bit()) {    // read complement bit
+        if (b)                // 11
+          return OW_DATA_ERR; // data error
+      } else {
+        if (!b) { // 00 = 2 devices
+          if (diff > i || ((*id & 1) && diff != i)) {
+            b = 1;         // now 1
+            next_diff = i; // next pass 0
+          }
+        }
+      }
+      ow_write_bit(b); // write bit
+      *id >>= 1;
+      if (b)
+        *id |= 0x80; // store bit
+      i--;
+    } while (--j);
+    id++; // next byte
+  } while (i);
+  return next_diff; // to continue search
+}
+
+void ow_find_rom(uint8_t *diff, uint8_t id[]) {
+  while (1) {
+    *diff = ow_search_rom(*diff, &id[0]);
+    if (*diff == OW_PRESENCE_ERR || *diff == OW_DATA_ERR ||
+        *diff == OW_LAST_DEVICE)
+      return;
+    // if ( id[0] == DS18B20_ID || id[0] == DS18S20_ID )
+    return;
+  }
+}
+
+uint8_t ow_read_rom(uint8_t *buffer) {
+  if (!ow_reset())
+    return 0;
+  ow_write_byte(OW_CMD_READROM);
+  for (uint8_t i = 0; i < 8; i++) {
+    buffer[i] = ow_read_byte();
+  }
+  return 1;
+}
+
+uint8_t ow_match_rom(uint8_t *rom) {
+  if (!ow_reset())
+    return 0;
+  ow_write_byte(OW_CMD_MATCHROM);
+  for (uint8_t i = 0; i < 8; i++) {
+    ow_write_byte(rom[i]);
+  }
+  return 1;
+}
---
+++ AVRTools/OneWire.h
@@ -0,0 +1,73 @@
+#ifndef ONEWIRE_H
+#define ONEWIRE_H
+
+#include "ArduinoPins.h"
+#include "GpioPinMacros.h"
+
+#define MAXDEVICES 10
+
+// USART-driven 1-wire emulation
+//#define UART_AS_OneWire
+
+// 2-pins emulation
+//#define OW_TWO_PINS
+
+#ifdef UART_AS_OneWire
+//#define USART_BAUDRATE_57600 (((F_CPU / (57600 * 16UL))) - 1)
+//#define USART_BAUDRATE_115200 (((F_CPU / (115200 * 16UL))) - 1)
+//#define USART_BAUDRATE_9600 (((F_CPU / (9600 * 16UL))) - 1)
+#else
+#include <util/delay.h>
+
+//#define OW_ALTER_PIN pPin12
+#ifndef OW_TWO_PINS
+#define OW_ALTER_PIN pPin12
+#else
+#define OW_ALTER_PIN_IN pPin12
+#define OW_ALTER_PIN_OUT pPin13
+#endif
+#endif
+
+#define OW_CMD_SEARCHROM 0xF0
+#define OW_CMD_READROM 0x33
+#define OW_CMD_MATCHROM 0x55
+#define OW_CMD_SKIPROM 0xCC
+
+#define OW_SEARCH_FIRST 0xFF // start new search
+#define OW_PRESENCE_ERR 0xFF
+#define OW_DATA_ERR 0xFE
+#define OW_LAST_DEVICE 0x00 // last device found
+//			0x01 ... 0x40: continue searching
+
+#define OW_DS1990_FAMILY_CODE 1
+#define OW_DS2405_FAMILY_CODE 5
+#define OW_DS2413_FAMILY_CODE 0x3A
+#define OW_DS1822_FAMILY_CODE 0x22
+#define OW_DS2430_FAMILY_CODE 0x14
+#define OW_DS1990_FAMILY_CODE 1
+#define OW_DS2431_FAMILY_CODE 0x2d
+#define OW_DS18S20_FAMILY_CODE 0x10
+#define OW_DS18B20_FAMILY_CODE 0x28
+#define OW_DS2433_FAMILY_CODE 0x23
+
+// rom-code size including CRC
+#define OW_ROMCODE_SIZE 8
+
+uint8_t ow_reset(void);
+void ow_write_bit(uint8_t bit);
+uint8_t ow_read_bit(void);
+
+#ifndef UART_AS_OneWire
+uint8_t ow_read_byte(void);
+void ow_write_byte(uint8_t byte);
+#else
+uint8_t ow_write_byte(uint8_t byte);
+#define ow_read_byte() ow_write_byte(0xFF)
+#endif
+
+uint8_t ow_search_rom(uint8_t diff, uint8_t *id);
+void ow_find_rom(uint8_t *diff, uint8_t id[]);
+uint8_t ow_read_rom(uint8_t *buffer);
+uint8_t ow_match_rom(uint8_t *rom);
+
+#endif
